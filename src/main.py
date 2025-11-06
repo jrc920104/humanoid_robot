a231fca6e7ee99f6f744998f808cd4c04b191f67
jiangruncheng <jiangruncheng@qutoutiao.net>
2025-11-06 15:00:42 +0800

    add

---
+++ src/main.py
@@ -0,0 +1,10 @@
+def robot_greeting():
+    """机器人问候函数"""
+    print("\n  ____  ")
+    print(" |  o|  Hello! 我是你的机器人助手")
+    print(" /___\\ ")
+    print("   |   ")
+    print("  / \\  Ready to learn robotics!")
+
+if __name__ == "__main__":
+    robot_greeting()
